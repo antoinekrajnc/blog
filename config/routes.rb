4ae6bfa593642b36b86e54f177b5871795f8c31c
antoinekrajnc <antoinekrajnc@gmail.com>
2017-05-26 14:56:38 -0700

    change sign out route V2

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   resources :posts
   get '/admin', to: 'posts#admin', as: 'admin'
-  delete '/admins/sign_out', to: 'posts#index'
+  delete '/admins/sign_out', to: 'devise/sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "posts#index"
 
